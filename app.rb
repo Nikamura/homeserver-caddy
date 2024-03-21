6de687f07bfe9652e812ad9e2483d581e001ccdd
Karolis Mazukna <k@karolis.dev>
2024-03-21 23:47:40 +0200

    Handle exceptions in update_caddyfile.rb to improve error handling

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require_relative "./update_caddyfile.rb"
 STDOUT.sync = true
 
 while true
-  update_caddyfile
+  begin
+    update_caddyfile
+  rescue => e
+    puts "Error: #{e.message}"
+    puts e.backtrace
+  end
   sleep ENV.fetch("CADDYFILE_GENERATOR_INTERVAL", 60).to_i
 end
